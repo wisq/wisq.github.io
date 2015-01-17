87def57b4bf6abd147c5d90b64bb714d460af2d3
Adrian Irving-Beer <wisq@wisq.net>
2015-01-17 04:18:55 -0500

    Switch to non-minified JS, CSS.  Add dynamic minifying.

--- _plugins/compress.rb
+++ _plugins/compress.rb
@@ -1,18 +1,16 @@
 require 'jekyll_asset_pipeline'
 
-# Right now, this just seems to make our JS *larger*.  At least, post-compression.
-#
-#class JavaScriptCompressor < JekyllAssetPipeline::Compressor
-#  require 'closure-compiler'
-#
-#  def self.filetype
-#    '.js'
-#  end
-#
-#  def compress
-#    return Closure::Compiler.new.compile(@content)
-#  end
-#end
+class JavaScriptCompressor < JekyllAssetPipeline::Compressor
+  require 'closure-compiler'
+
+  def self.filetype
+    '.js'
+  end
+
+  def compress
+    return Closure::Compiler.new.compile(@content)
+  end
+end
 
 module JekyllAssetPipeline
   class CssCompressor < JekyllAssetPipeline::Compressor
